cc5c8cfeeba14984f0ecbd5f51c7de1026a19e9d
wasim <wasimsidan785@gmail.com>
2026-02-15 03:56:21 +0530

    fifth change

---
+++ terraform/terraform.tfvars
@@ -0,0 +1,3 @@
+aws_region = "us-east-1"
+bucketname = "jennkins-2026"
+environment = "prod"
---
+++ terraform/variables.tf
@@ -0,0 +1,11 @@
+variable "aws_region" {
+    description = "aws region"
+}
+
+variable "bucketname" {
+    description = "bucketname"
+}
+
+variable "environment" {
+    description = "environment"
+}
