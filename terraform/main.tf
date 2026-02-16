9f2b57126d24d27bccc601a9761ad693d792b227
wasim <wasimsidan785@gmail.com>
2026-02-17 02:25:46 +0530

    twelve change

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,17 +1,17 @@
-resource "aws_s3_bucket" "static-website" {
-  bucket = "var.bucketname"
+resource "aws_s3_bucket" "was-sid-static-website-build-2026" {
+  bucket = var.bucketname
 
 }
 
 resource "aws_s3_bucket_website_configuration" "website-config" {
-    bucket = aws_s3_bucket.static-website.id
+    bucket = aws_s3_bucket.was-sid-static-website-build-2026.id
     index_document {
       suffix = "index.html"
     }
 }
 
 resource "aws_s3_object" "index" {
-    bucket = aws_s3_bucket.static-website.bucket
+    bucket = aws_s3_bucket.was-sid-static-website-build-2026.bucket
     key = "index.html"
     source = "/c/Users/Sidan/Desktop/static-website/index.html"
     content_type = "text/html"
