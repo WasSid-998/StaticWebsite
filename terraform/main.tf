187bf0554552e53b8993a2c1f5c81af56eb3134f
wasim <wasimsidan785@gmail.com>
2026-02-17 05:20:59 +0530

    9 change

--- terraform/main.tf
+++ terraform/main.tf
@@ -16,3 +16,29 @@ resource "aws_s3_object" "index" {
     source = "/app/website/index.html"
     content_type = "text/html"
 }
+
+resource "aws_s3_bucket_policy" "public_policy" {
+  bucket = aws_s3_bucket.was-sid-static-1998-unique-website-build-2026.id
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Sid       = "PublicReadGetObject"
+        Effect    = "Allow"
+        Principal = "*"
+        Action    = ["s3:GetObject"]
+        Resource  = "${aws_s3_bucket.was-sid-static-1998-unique-website-build-2026.arn}/*"
+      }
+    ]
+  })
+}
+
+resource "aws_s3_bucket_public_access_block" "block" {
+  bucket = aws_s3_bucket.was-sid-static-1998-unique-website-build-2026.id
+
+  block_public_acls       = false
+  block_public_policy     = false
+  ignore_public_acls      = false
+  restrict_public_buckets = false
+}
